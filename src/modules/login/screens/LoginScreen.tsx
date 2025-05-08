c515726750e7ee3843e8aad6b565b4e5cac3db43
Bella-Farias <belllafarias99@gmail.com>
2025-05-08 20:03:56 -0300

    feat: insert state in login

--- src/modules/login/screens/LoginScreen.tsx
+++ src/modules/login/screens/LoginScreen.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Button from '../../../shared/buttons/button default/Button';
 import Input from '../../../shared/inputs/input default/input';
 import {
@@ -10,6 +11,19 @@ import {
 } from '../styles/loginScreen.styles';
 
 const LoginScreen = () => {
+  const [username /*É o valor do State */, setUserName /*Função que vai alterar o valor do State */] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUserName = (event: React.ChangeEvent<HTMLInputElement>) => {
+    //A const "handleUserName" foi criada para atualizar o State "username" toda vez que o usuario digitar algo no input. Se não fizer isso o React não sabe o que está sendo digitado, consequentimente não conseguindo usar o valor digitado depois (como por exemplo para fazer login, validar campos, etc).
+    setUserName(event.target.value);
+    //Atualiza o valor do "usernam" com o que foi digitado.
+  };
+
+  const handlePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  }
+
   return (
     <ContainerLoginScreen>
       <BackgroundImage src="./background.png" />
@@ -17,9 +31,11 @@ const LoginScreen = () => {
         <LimitedContainer>
           <LogoImage src="./logo.png" />
           <TitleLogin>LOGIN</TitleLogin>
-          <Input title="USUÁRIO:" />
-          <Input title="SENHA:" />
-          <Button type='primary' margin="64px 0px 16px 0px">ENTRAR</Button>
+          <Input title="USUÁRIO:" margin="32px 0px 0px" onChange={handleUserName} value={username} /*O valor do input será sempre o que estiver no State "username"*/ />
+          <Input type='password' title="SENHA:" margin="32px 0px 0px" onChange={handlePassword} value={password} />
+          <Button type="primary" margin="64px 0px 16px 0px">
+            ENTRAR
+          </Button>
         </LimitedContainer>
       </ContainerLogin>
     </ContainerLoginScreen>
--- src/shared/inputs/input default/input.tsx
+++ src/shared/inputs/input default/input.tsx
@@ -4,11 +4,12 @@ import { BoxInput, TitleInput } from './input.styles';
 interface InputProps extends InputPropsAntd {
   title?: string;
   //Define que o componente pode receber uma prop "title", tipo string, e ela é opcional (por causa do ?); “A interface InputProps herda todas as propriedades que um input normal pode ter, e também pode ter uma propriedade title que é minha personalizada.
+  margin?: string;
 }
 
-const Input = ({ title, ...props }: InputProps) => {
+const Input = ({ title, margin, ...props }: InputProps) => {
   return (
-    <BoxInput>
+    <BoxInput style={{margin}}>
       {title && <TitleInput>{title}</TitleInput>}
       <InputAntd {...props} />
     </BoxInput>
